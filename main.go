afa66adad1ee123c2411abcd87ac2e3e3a4deee5
KostaLosta <macbook@Air-de-Tery.Dlink>
2023-05-25 16:15:37 +0300

    very bad

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 	"strconv"
 )
 
-func Calculator() {
+func Scan() {
 
 	var name string
 	fmt.Println("Как тебя зовут?")
@@ -17,9 +16,7 @@ func Calculator() {
 	var one, two, sum string
 
 	fmt.Println("Введите число 1:")
-	fmt.Scanf("%d\n", &one)
-
-	fmt.Println(reflect.TypeOf(one))
+	fmt.Scanf("%s\n", &one)
 
 	oneInt, err := strconv.Atoi(one)
 	if err != nil {
@@ -27,39 +24,35 @@ func Calculator() {
 		fmt.Scanf("%s\n", &one)
 	}
 
-	fmt.Println(reflect.TypeOf(oneInt))
-
 	var symbol string
 	fmt.Println("Введите арифметическое действие:")
 	fmt.Scanf("%s\n", &symbol)
 
 	fmt.Println("Введите число 2:")
-	fmt.Scanf("%d\n", &two)
+	fmt.Scanf("%s\n", &two)
 
-	fmt.Println(reflect.TypeOf(two))
-
-	//twoInt, err := strconv.Atoi(two)
-	//if err != nil {
-	//	fmt.Println("ошибка, введите число повторно")
-	//	fmt.Scanf("%s\n", &two)
-	//}
-	//fmt.Println(reflect.TypeOf(twoInt))
+	twoInt, err := strconv.Atoi(two)
+	if err != nil {
+		fmt.Println("ошибка, введите число повторно")
+		fmt.Scanf("%s\n", &two)
+	}
 
 	switch symbol {
 	case "+":
 		sum = one + two
-		fmt.Println(name, ", Ваш результат", sum)
 	case "-":
 		sum = one - two
-		fmt.Println(name, ", Ваш результат", sum)
 	case "*":
 		sum = one * two
-		fmt.Println(name, ", Ваш результат", sum)
 	case "/":
 		sum = one / two
-		fmt.Println(name, ", Ваш результат", sum)
+	default:
+		fmt.Println("ОШИБКА")
 	}
+	fmt.Println(sum)
 }
+
 func main() {
-	Calculator()
+	Scan()
+
 }
