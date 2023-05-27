40a51fceb0efee5ec4d27e1defdedcb5fb1b0748
KostaLosta <macbook@MacBookstaLosta.Dlink>
2023-05-27 15:29:18 +0300

    посмотри, ошибка в возврате значения в функции calc

--- main.go
+++ main.go
@@ -1,73 +1,18 @@
 package main
 
 import (
-	"Calculator/counter"
 	"fmt"
 	"strconv"
 )
 
-//
-// func Scan() {   задаа функции получить значения от пользователя,  должна вернуть значения какие...
-//
-// var name string
-// fmt.Println("Как тебя зовут?")
-// fmt.Scanf("%s\n", &name)
-//
-// fmt.Println("Вашему вниманию предоставляется универсальный калькулятор, разработанный начинающим программистом")
-//
-// var one, two string
-// var sum int
-// fmt.Println("Введите число 1:")
-// fmt.Scanf("%s\n", &one)
-//
-// oneInt, err := strconv.Atoi(one)
-// if err != nil {
-// fmt.Println("ошибка, введите число повторно")
-// fmt.Scanf("%s\n", &one)
-// }
-// var symbol string
-// fmt.Println("Введите арифметическое действие:")
-// fmt.Scanf("%s\n", &symbol)
-//
-// fmt.Println("Введите число 2:")
-// fmt.Scanf("%s\n", &two)
-//
-// twoInt, err := strconv.Atoi(two)
-// if err != nil {
-// fmt.Println("ошибка, введите число повторно")
-// fmt.Scanf("%s\n", &two)
-// }
-//
-// }
-
-// func calc(){  //   задача фукнции  значения получить, производит операцию, значение  вернуть
-// switch symbol {
-// case "+":
-// sum = oneInt + twoInt
-// case "-":
-// sum = oneInt - twoInt
-// case "*":
-// sum = oneInt * twoInt
-// case "/":
-// sum = oneInt / twoInt
-// default:
-// fmt.Println("ОШИБКА, неверное указано логарифмическое действие")
-// }
-// fmt.Println(sum)
-//
-//
-// }
-
-func Scan() { // почему опять одна функция которая выполняет две операции?? мы же так не договаривались
+func Scan() (int, int, string) {
 
 	var name string
-	fmt.Println("Как тебя зовут?")
+	fmt.Println("Вашему вниманию представляется универсальный калькуляторю. Ведите Ваше имя:")
 	fmt.Scanf("%s\n", &name)
 
-	fmt.Println("Вашему вниманию предоставляется универсальный калькулятор, разработанный начинающим программистом")
-
 	var one, two string
-	var sum int
+
 	fmt.Println("Введите число 1:")
 	fmt.Scanf("%s\n", &one)
 
@@ -76,6 +21,7 @@ func Scan() { // почему опять одна функция которая 
 		fmt.Println("ошибка, введите число повторно")
 		fmt.Scanf("%s\n", &one)
 	}
+
 	var symbol string
 	fmt.Println("Введите арифметическое действие:")
 	fmt.Scanf("%s\n", &symbol)
@@ -88,6 +34,13 @@ func Scan() { // почему опять одна функция которая 
 		fmt.Println("ошибка, введите число повторно")
 		fmt.Scanf("%s\n", &two)
 	}
+	return oneInt, twoInt, symbol
+
+}
+
+func calc(oneInt, twoInt int, symbol string) int {
+
+	var sum int
 
 	switch symbol {
 	case "+":
@@ -101,8 +54,10 @@ func Scan() { // почему опять одна функция которая 
 	default:
 		fmt.Println("ОШИБКА, неверное указано логарифмическое действие")
 	}
-	fmt.Println(sum)
+	return sum
+
 }
+
 func main() {
 	// ответ почему у тебя не получилось, в документации есть как создать проект в go
 	// https://go.dev/doc/code
@@ -113,11 +68,11 @@ func main() {
 
 	// что с этим делать, ответ это тоже просто запоминается на изусть действия при создании проекта на go
 
-	counter.CreatedPackageForBro()
+	//counter.CreatedPackageForBro()
 
 	Scan() //  return 3 variables
-	counter.CreatedPackageForBro()
-
+	//counter.CreatedPackageForBro()
+	calc()
 	// calc() // return 1 variables
 
 	// создать строку записи
